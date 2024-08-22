d0091ec15b7afa3e8a055230fc22d89c4c427fd7
evil-89789 <tngsurya055@gmail.com>
2024-08-22 11:36:15 +0545

    express js removed

--- server/connection/connectDb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-//  **************************/ function to connect to database \******************************\\
-
-const connectToDb = async (url) => {
-  return mongoose.connect(url);
-};
-module.exports = { connectToDb };
--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const users = require("../models/model");
-const getAllUsers = (req, res) => {
-  const allUsers = users.find({});
-  res.json(users);
-};
-
-module.exports = {
-  getAllUsers,
-};
