37b052506084a3d2e51024fa40d040915a2376a5
evil-89789 <tngsurya055@gmail.com>
2024-08-24 09:09:23 +0545

    revenue page

--- client/src/pages/Revenue.jsx
+++ client/src/pages/Revenue.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/revenue.css";
 
 const Revenue = () => {
-  return <div></div>;
+  const [revenue, setRevenue] = useState({
+    remark: "",
+    amount: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRevenue({ ...revenue, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(revenue);
+  };
+  return (
+    <section className="revenue_section">
+      <h1>Add revenue</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={revenue.remark}
+          onChange={handleChange}
+          name="remark"
+          placeholder="remarks"
+        />
+        <input
+          type="number"
+          name="amount"
+          value={revenue.amount}
+          onChange={handleChange}
+          placeholder="amount"
+        />
+        <button>Add</button>
+      </form>
+    </section>
+  );
 };
 
 export default Revenue;
