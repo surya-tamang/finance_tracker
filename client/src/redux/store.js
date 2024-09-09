bfbbf09be5e58514559b90947c3bf1ece17ffc7c
evil-89789 <tngsurya055@gmail.com>
2024-09-09 18:01:41 +0545

    store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import blcReducer from "./slices/blcSlice";
 import expenseReducer from "./slices/expenseSlice";
 import usersReducer from "./slices/users";
-import users from "./slices/users";
+import addUserReducer from "./slices/addUser";
 
 const store = configureStore({
   reducer: {
     balance: blcReducer,
     expense: expenseReducer,
     users: usersReducer,
+    addUser: addUserReducer,
   },
 });
 
